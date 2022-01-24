cc73f434c5300c0ba6351d355f9d1483500bad53
TimeJumper1 <benassai.stefano.1@gmail.com>
2022-01-24 17:30:10 +0100

    snack 4 completato

---
+++ snack-4/index.php
@@ -0,0 +1,5 @@
+<?php 
+$testo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quas dolor ut eaque molestias quia beatae reprehenderit velit non ullam odit quam dolore provident, officiis, at dolores quidem aliquid eum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto fugiat libero commodi voluptates, blanditiis molestiae magnam cum quia amet deleniti sequi neque id error ullam iusto minima, rerum voluptatem nulla. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis repudiandae, minima ipsam ex quas cupiditate. Modi, voluptates eveniet? Dolores, recusandae. Alias, eveniet totam? Aliquid fuga ex nulla alias corporis pariatur.';
+$result = explode('.',$testo);
+var_dump($result);
+?>
